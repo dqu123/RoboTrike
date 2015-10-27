4e038d32e9c1067e514c2e552cc997bcc8ebc582
David Qu <davidqu12345@gmail.com>
2015-10-27 14:05:57 -0700

    Added hw2 code.

---
+++ hw2/clean.bat
@@ -0,0 +1,1 @@
+del *.obj, *.lnk, *.mp*, *~, *.lst, hw2test
---
+++ hw2/make.bat
@@ -0,0 +1,5 @@
+asm86chk converts.asm
+asm86 converts.asm m1 ep db
+asm86 hw2test.asm m1 ep db
+link86 hw2test.obj, converts.obj to hw2test.lnk
+loc86 hw2test.lnk NOIC AD(SM(CODE(4000H),DATA(400H),STACK(7000H)))
